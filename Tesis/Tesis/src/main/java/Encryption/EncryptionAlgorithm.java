3c963b3d870213e3138bad73bca734f31c087265
RobertoAnabalon <144553669+RobertoAnabalon@users.noreply.github.com>
2024-11-12 11:12:59 -0300

    Add files via upload

---
+++ Tesis/Tesis/src/main/java/Encryption/EncryptionAlgorithm.java
@@ -0,0 +1,8 @@
+package main.java.Encryption;
+
+public interface EncryptionAlgorithm {
+    
+    String encrypt(String data) throws Exception;
+    String decrypt(String data) throws Exception;
+}
+
